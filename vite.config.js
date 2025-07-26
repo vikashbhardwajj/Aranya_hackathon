eb67b3c1bb8b318d050b34544e60704524857923
Vikash Bhardwaj <ind.vikashbhardwaj@gmail.com>
2025-07-26 12:19:07 +0530

    locol port deleted

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    open: true
-  }
-  
-  
 });
